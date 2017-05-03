3a1a8909bf3eb795ba8a76af4816115af34a591f
章礼平 <LipperZack@gmail.com>
2017-05-04 00:04:12 +0800

    + [dnd](drop) bind DropTarget to Stage

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,15 +1,38 @@
-import React, { Component } from 'react';
-import { DragDropContext } from 'react-dnd';
+import React, { Component, PropTypes } from 'react';
+import { DragDropContext, DropTarget } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
+import _ from 'lodash';
+import { ItemTypes } from '../cons/Constants';
 
 import Node from './Node';
 
+const stageTarget = {
+  drop(props) {
+    const Console = console;
+    Console.log(props.x, props.y);
+  }
+};
+
+function collect(connect, monitor) {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver()
+  };
+}
+
 class Stage extends Component {
   render() {
-    return (
-      <Node />
+    const { connectDropTarget } = this.props;
+    return connectDropTarget(
+      <div>
+        <Node />
+      </div>
     );
   }
 }
 
-export default DragDropContext(HTML5Backend)(Stage);
+Stage.propTypes = {
+  connectDropTarget: PropTypes.func.isRequired
+};
+
+export default _.flow((DropTarget(ItemTypes.Node, stageTarget, collect)), DragDropContext(HTML5Backend))(Stage);
