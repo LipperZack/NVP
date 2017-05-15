aed10b2c3af3fede4414018d2f08e613117b23c0
章礼平 <LipperZack@gmail.com>
2017-05-15 10:20:07 +0800

    + [redux](Store) Instantiate Store and Provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
+
 import Stage from './components/Stage';
 import './index.css';
 
+const store = configureStore();
+
 
 ReactDOM.render(
-  <Stage />,
+  <Provider store={store}>
+    <Stage />
+  </Provider>,
   document.getElementById('root')
 );
